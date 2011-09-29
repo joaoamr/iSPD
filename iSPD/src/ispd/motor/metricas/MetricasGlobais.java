f5027a278d996f48c8db75ac412497704f9d932a
denison <denison@e2395cfc-e4cf-471b-bddb-e734dad3db67>
2011-09-29 18:03:44 +0000

    git-svn-id: https://gspd.dcce.ibilce.unesp.br/ispd@52 e2395cfc-e4cf-471b-bddb-e734dad3db67

--- iSPD/src/ispd/motor/metricas/MetricasGlobais.java
+++ iSPD/src/ispd/motor/metricas/MetricasGlobais.java
@@ -6,6 +6,8 @@
 package ispd.motor.metricas;
 
 import ispd.motor.filas.RedeDeFilas;
+import ispd.motor.filas.servidores.CS_Comunicacao;
+import ispd.motor.filas.servidores.CS_Processamento;
 
 /**
  *
@@ -14,28 +16,76 @@ import ispd.motor.filas.RedeDeFilas;
 public class MetricasGlobais {
     private double tempoSimulacao;
     private double satisfacaoMedia;
-    private double ociosidadeMedia;
+    private double ociosidadeCompuacao;
+    private double ociosidadeComunicacao;
     private double eficiencia;
     
     public MetricasGlobais(RedeDeFilas redeDeFilas, double tempoSimulacao){
         this.tempoSimulacao = tempoSimulacao;
         this.satisfacaoMedia = 100;
+        this.ociosidadeCompuacao = getOciosidadeComputacao(redeDeFilas);
+        this.ociosidadeComunicacao = getOciosidadeComunicacao(redeDeFilas);
+        this.eficiencia = getEficiencia(redeDeFilas);
     }
 
     public double getEficiencia() {
         return eficiencia;
     }
 
-    public double getOciosidadeMedia() {
-        return ociosidadeMedia;
+    public double getOciosidadeCompuacao() {
+        return ociosidadeCompuacao;
     }
 
+    public double getOciosidadeComunicacao() {
+        return ociosidadeComunicacao;
+    }
+    
     public double getSatisfacaoMedia() {
         return satisfacaoMedia;
     }
 
     public double getTempoSimulacao() {
         return tempoSimulacao;
+    }
+
+    private double getOciosidadeComputacao(RedeDeFilas redeDeFilas) {
+        double tempoLivreMedio = 0.0;
+        for (CS_Processamento maquina : redeDeFilas.getMaquinas()) {
+            double aux = maquina.getMetrica().getSegundosDeProcessamento();
+            aux = (this.getTempoSimulacao() - aux);
+            tempoLivreMedio += aux;//tempo livre
+            aux = maquina.getOcupacao() * aux;
+            tempoLivreMedio -= aux;
+        }
+        tempoLivreMedio = tempoLivreMedio / redeDeFilas.getMaquinas().size();
+        return (tempoLivreMedio * 100) / getTempoSimulacao();
+    }
+    
+    private double getOciosidadeComunicacao(RedeDeFilas redeDeFilas) {
+        double tempoLivreMedio = 0.0;
+        for (CS_Comunicacao link : redeDeFilas.getLinks()) {
+            double aux = link.getMetrica().getSegundosDeTransmissao();
+            aux = (this.getTempoSimulacao() - aux);
+            tempoLivreMedio += aux; //tempo livre
+            aux = link.getOcupacao() * aux;
+            tempoLivreMedio -= aux;
+        }
+        tempoLivreMedio = tempoLivreMedio / redeDeFilas.getLinks().size();
+        return (tempoLivreMedio * 100) / getTempoSimulacao();
+    }
+
+    private double getEficiencia(RedeDeFilas redeDeFilas) {
+        double tempoUtil = 0.0;
+        double tempoMedio = 0.0;
+        for (CS_Processamento maquina : redeDeFilas.getMaquinas()) {
+            double aux = maquina.getMetrica().getSegundosDeProcessamento();
+            aux = (this.getTempoSimulacao() - aux);//tempo livre
+            aux = maquina.getOcupacao() * aux;//tempo processando sem ser tarefa
+            tempoUtil = aux + maquina.getMetrica().getSegundosDeProcessamento();
+            tempoMedio += tempoUtil / this.getTempoSimulacao();
+        }
+        tempoMedio = tempoMedio / redeDeFilas.getMaquinas().size();
+        return tempoMedio;
     }
     
 }
