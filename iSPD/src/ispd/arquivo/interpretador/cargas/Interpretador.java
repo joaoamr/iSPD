49db40c76257cbe1cf6d294bf52b9b9d44f04f13
diogots <diogots@e2395cfc-e4cf-471b-bddb-e734dad3db67>
2012-10-30 02:09:30 +0000

    git-svn-id: https://gspd.dcce.ibilce.unesp.br/ispd@116 e2395cfc-e4cf-471b-bddb-e734dad3db67

--- iSPD/src/ispd/arquivo/interpretador/cargas/Interpretador.java
+++ iSPD/src/ispd/arquivo/interpretador/cargas/Interpretador.java
@@ -6,103 +6,176 @@ package ispd.arquivo.interpretador.cargas;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Diogo Tavares
  */
 public class Interpretador {
-    /**
-     * 
-     * @param args the command line arguments
-     */
-     /*public static void main(String[] args) {
-        // TODO code application logic here
-         Interpretador inter;
-         String caminho, tipo, saida;
-         
-         inter = new Interpretador();
-         caminho="nordugrid.gwf";
-         tipo="GWF";
-         saida="nordugrid";
-         inter.convert(caminho,tipo,saida);
-         System.out.println("Arquivo "+saida+".wmsx gerado");
-    }*/
-     
-    public void convert(String caminho, String tipo, String saida){    
-        // TODO code application logic here
-        try {   BufferedReader in = new BufferedReader(new FileReader(caminho));
-                BufferedWriter out = new BufferedWriter(new FileWriter(saida+".wmsx"));
-                String str;
-                //iniciando a escrita do arquivo
-                out.write("<?xml version=\"1.0\" encoding=\"ISO-8859-1\" standalone=\"no\"?>\n" +
-                          "<!DOCTYPE system SYSTEM \"iSPDcarga.dtd\">");
-                out.write("\n<load>");
-                out.write("\n<trace>");
-                out.write("\n<format kind=\""+tipo+"\" />");
-                while (in.ready()) {
-                    
-                    str = in.readLine();
-                    if(str.equals("")){/*System.out.println("linha em branco");*/}
-                    else if("SWF".equals(tipo)){
-                        p_tasksSWF(str,out);
-                    }
-                    else if("GWF".equals(tipo)){
-                        p_tasksGWF(str,out);
-                    }                    
-                }    
-                out.write("\n</trace>");
-                out.write("\n</load>");
-                
-                //FECHANDO ARQUIVOS
-                
-                in.close();
-                out.close();
-        }
-        catch (IOException e) {}
+
+    private String caminho, tipo, saida;
+    private int num_tasks;
+
+    public Interpretador(String caminho) {
+        this.caminho = caminho;
+        String aux;
+        int i = caminho.lastIndexOf(".");
+        this.saida = (caminho.substring(0, i)+".wmsx");
+        this.tipo = caminho.substring(i + 1).toUpperCase();
+        System.out.println(this.caminho + "-" + this.saida + "-" + this.tipo);
     }
 
+    public String getSaida(){
+        return this.saida;
+    }
+    
+    public void convert() {
+        // TODO code application logic here
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(caminho));
+            BufferedWriter out = new BufferedWriter(new FileWriter(saida));
+            String str;
+            int task1_arrive = 0;
+            boolean flag = false;
+            //iniciando a escrita do arquivo
+            out.write("<?xml version=\"1.0\" encoding=\"ISO-8859-1\" standalone=\"no\"?>\n"
+                    + "<!DOCTYPE system SYSTEM \"iSPDcarga.dtd\">");
+            out.write("\n<system>");
+            out.write("\n<trace>");
+            out.write("\n<format kind=\"" + tipo + "\" />");
+            int i = 0;
+            while (in.ready()) {
+                str = in.readLine();
+                if (str.equals("") || str.charAt(0) == ';' || str.charAt(0) == '#') {/*System.out.println("linha em branco");*/
 
-    private static void p_tasksGWF(String str,BufferedWriter out) throws IOException{
-        if(str.charAt(0)=='#'){/*System.out.println("Comentário:"+str);*/}//Verifica se é linha de comentário, para os casos do SWF e GWF
-        else{
+                } else if ("SWF".equals(tipo)) {
+                    p_tasksSWF(str, out);
+                    i++;
+                } else if ("GWF".equals(tipo)) {
+                    if (!flag) {
+                        task1_arrive = acha1task(str);
+                        flag = true;
+                        p_tasksGWF(str, out, task1_arrive);
+                        i++;
+                    } else {
+                        p_tasksGWF(str, out, task1_arrive);
+                        i++;
+                    }
+                }
+            }
+            out.write("\n</trace>");
+            out.write("\n</system>");
+
+            //FECHANDO ARQUIVOS
+            num_tasks = i;
+            in.close();
+            out.close();
+        } catch (IOException e) {
+        }
+    }
+
+    private static void p_tasksGWF(String str, BufferedWriter out, int first_task) throws IOException {
+        if (str.charAt(0) == '#') {
+            System.out.println(/*"Comentário:"+str*/);
+        }//Verifica se é linha de comentário, para os casos do SWF e GWF
+        else {
             //System.out.println(str);
-            str = str.replaceAll("\t"," ");//elimina espaços em branco desnecessários
+            str = str.replaceAll("\t", " ");//elimina espaços em branco desnecessários
             str = str.trim();
             //System.out.println(str);
-            
+
             String[] campos = str.split(" ");
-            if(campos[3].equals("-1")){}
-            else{
+            if (campos[3].equals("-1")) {
+            } else {
                 out.write("\n<task ");
-                out.write("id=" + "\"tsk"+campos[0]+ 
-                            "\" arr=\"" + campos[1]+ 
-                            "\" sts=\"" + campos[10]+
-                            "\" cpsz =\"" + campos[3] +
-                            "\" cmsz=\"" + campos[20] +
-                            "\" usr=\"" + campos[11]);
+                out.write("id=" + "\"" + campos[0]
+                        + "\" arr=\"" + (Integer.parseInt(campos[1]) - first_task)
+                        + "\" sts=\"" + campos[10]
+                        + "\" cpsz =\"" + campos[3]
+                        + "\" cmsz=\"" + campos[20]
+                        + "\" usr=\"" + campos[11]);
                 out.write("\" />");
             }
         }
     }
-    
+
     private static void p_tasksSWF(String str, BufferedWriter out) throws IOException {
-        if(str.charAt(0) == ';'){/*ignore*/} //Verifica se é linha de comentário, para os casos do SWF e GWF
-        else{
-            str = str.replaceAll("\\s\\s++"," ");//elimina espaços em branco desnecessários
+        if (str.charAt(0) == ';') {/*ignore*/
+
+        } //Verifica se é linha de comentário, para os casos do SWF e GWF
+        else {
+            str = str.replaceAll("\\s\\s++", " ");//elimina espaços em branco desnecessários
             str = str.trim();
             out.write("\n<task ");
             String[] campos = str.split(" ");
-            out.write("id=" + "\"task"+campos[0]+ 
-                            "\" arrival=\"" + campos[1]+ 
-                            "\" status=\"" + campos[10]+
-                            "\" comp_size =\"" + campos[3] +
-                            "\" comm_size=\"-1" +
-                            "\" user=\"" + "user"+campos[11]);
+            out.write("id=" + "\"" + campos[0]
+                    + "\" arr=\"" + campos[1]
+                    + "\" sts=\"" + campos[10]
+                    + "\" cpsz =\"" + campos[3]
+                    + "\" cmsz=\"-1"
+                    + "\" usr=\"" + "user" + campos[11]);
             out.write("\" />");
         }
     }
+
+    private int acha1task(String str) {
+        str = str.replaceAll("\t", " ");//elimina tabs desnecessários
+        str = str.trim();
+        //System.out.println(str);
+        String[] campos = str.split(" ");
+        int a = Integer.parseInt(campos[1]);
+        System.out.println(a);
+        return a;
+    }
+
+    @Override
+    public String toString() {
+        int i=0;
+        i=saida.lastIndexOf("\\");
+        saida=saida.substring(i+1);
+        return ("Arquivo " + saida + ".wmsx gerado com sucesso:\n"
+                + "\t- Gerado a partir de formato " + tipo
+                + "\n\t- Possui carga de " + num_tasks + " tarefas");
+                      
+    }
+
+    public String LerCargaWMS(){
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(caminho));
+            String texto="";
+            int j=0;
+            j=caminho.lastIndexOf("\\");
+            //pega o nome do arquivo
+            String nome;
+            nome=caminho.substring(j+1);
+            texto=texto.concat("Arquivo "+nome+" aberto com sucesso:\n");
+            String aux = null;
+            int i = 0;
+            while (in.ready()) {
+                aux = in.readLine();
+                if (i == 4) {
+                    String[] campos = aux.split(" ");
+                    campos = campos[1].split("\"");
+                    texto = texto.concat("\t- Arquivo extraído de trace no formato "+campos[1].toUpperCase()+"\n");
+                }
+                i++;
+            }
+            //desconta as 7 linhas de tags que não são tarefas..
+            i-=7;
+            texto = texto.concat("\t- Possui carga de "+i+" tarefas");
+            return (texto);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Interpretador.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        return ("Arquivo mal formatado");
+    }
 }
+//sem usr XML
+
