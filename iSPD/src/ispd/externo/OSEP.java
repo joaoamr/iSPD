3fd076f62011ad7ce003cf1c1b0e4ee98be596a1
cassio <cassio@e2395cfc-e4cf-471b-bddb-e734dad3db67>
2012-11-26 18:30:18 +0000

    git-svn-id: https://gspd.dcce.ibilce.unesp.br/ispd@128 e2395cfc-e4cf-471b-bddb-e734dad3db67

--- iSPD/src/ispd/externo/OSEP.java
+++ iSPD/src/ispd/externo/OSEP.java
@@ -38,6 +38,7 @@ public class OSEP extends Escalonador {
             indexOwner = metricaUsuarios.getUsuarios().indexOf(maq.getProprietario());
             creditos.get(indexUser).AtualizaUsado(tarefa.getTamProcessamento());
             creditos.get(indexOwner).AtualizaDoado(tarefa.getTamProcessamento());
+            creditos.get(indexUser).RemoveAtual(maq.getPoderComputacional());
         }
         maq.getInformacaoDinamicaProcessador().remove(tarefa);
         System.out.println("Tarefa " + tarefa.getIdentificador() + " do user "+tarefa.getProprietario()+" concluida "+mestre.getSimulacao().getTime());
@@ -47,6 +48,12 @@ public class OSEP extends Escalonador {
     public void adicionarTarefa(Tarefa tarefa){
         super.adicionarTarefa(tarefa);
         System.out.println("Tarefa " + tarefa.getIdentificador() + " do user "+tarefa.getProprietario()+" chegou "+mestre.getSimulacao().getTime());
+        CS_Processamento maq = (CS_Processamento) tarefa.getLocalProcessamento();
+        int indexUser;
+        if(tarefa.getLocalProcessamento() == null){
+            indexUser = metricaUsuarios.getUsuarios().indexOf(tarefa.getProprietario());
+            creditos.get(indexUser).RemoveAtual(maq.getPoderComputacional());
+        }
     }
 
     @Override
@@ -74,15 +81,68 @@ public class OSEP extends Escalonador {
                 }
             }
             tarefas.remove(selec);
+            selec.getProprietario();
             return selec;
         } else {
-            //modificar...
+            double total = 0.0;     
+            int usuario = -1;
+            for(i = 0; i<creditos.size();i++){
+                if(total < creditos.get(i).GetAtual() -  metricaUsuarios.getPoderComputacional(creditos.get(i).GetUsuario())){
+                    total = creditos.get(i).GetAtual() - metricaUsuarios.getPoderComputacional(creditos.get(i).GetUsuario());
+                    usuario = i;
+                }
+            }
+            if(usuario == -1){
+                return null;
+            }
+            else{
+                Tarefa selec = tarefas.get(0);
+            for (i = 1; i < tarefas.size(); i++) {
+                int indexSelec = metricaUsuarios.getUsuarios().indexOf(selec.getProprietario());
+                int indexTar = metricaUsuarios.getUsuarios().indexOf(tarefas.get(i).getProprietario());
+                if (creditos.get(indexTar).GetCredito() > creditos.get(indexSelec).GetCredito()) {
+                    selec = tarefas.get(i);
+                } else if (creditos.get(indexTar).GetCredito() == creditos.get(indexSelec).GetCredito() && tarefas.get(i).getTimeCriacao() < selec.getTimeCriacao()) {
+                    selec = tarefas.get(i);
+                }
+            }
+            if(creditos.get(metricaUsuarios.getUsuarios().indexOf(selec.getProprietario())).GetAtual() - metricaUsuarios.getPoderComputacional(selec.getProprietario()) > 0){
+                return null;
+            }
+            Double creditoTar = creditos.get(metricaUsuarios.getUsuarios().indexOf(selec.getProprietario())).GetCredito();
+            CS_Processamento rec;
+            rec = null;
+            for (i = 1; i < escravos.size(); i++) {
+                if(escravos.get(i).getProprietario() == creditos.get(usuario).GetUsuario()){
+                    Tarefa tar = (Tarefa) escravos.get(i).getInformacaoDinamicaProcessador().get(0);
+                int indexEscravo = metricaUsuarios.getUsuarios().indexOf(tar.getProprietario());
+                if (creditoTar > creditos.get(indexEscravo).GetCredito()) {
+                    if (rec == null) {
+                        rec = escravos.get(i);
+                    } else if (Math.abs(escravos.get(i).getPoderComputacional() - tarefaSelec.getTamProcessamento())
+                            < Math.abs(rec.getPoderComputacional() - tarefaSelec.getTamProcessamento())) {
+                        rec = escravos.get(i);
+                    }
+                }
+                }
+                
+                
+            }
+            if((creditos.get(usuario).GetAtual() - rec.getPoderComputacional()) - metricaUsuarios.getPoderComputacional(creditos.get(usuario).GetUsuario()) >= 0){
+                mestre.enviarMensagem( (Tarefa) rec.getInformacaoDinamicaProcessador().get(0),rec, Mensagens.DEVOLVER_COM_PREEMPCAO);
+                rec.getInformacaoDinamicaProcessador().remove(0);
+                tarefas.remove(selec);
+                return selec;
+            }
+                    
+            }
             //mestre.enviarMensagem(tarefa em execução, escravo selecionado, Mensagens.DEVOLVER_COM_PREEMPCAO);
             return null;
         }
 
     }
-
+    
+    
     @Override
     public CS_Processamento escalonarRecurso() {
         int i;
@@ -106,7 +166,7 @@ public class OSEP extends Escalonador {
             }
             //Buscando tarefa com menor prioridade
             Double creditoTar = creditos.get(metricaUsuarios.getUsuarios().indexOf(tarefaSelec.getProprietario())).GetCredito();
-            for (i = 1; i < escravos.size(); i++) {
+        for (i = 1; i < escravos.size(); i++){
                 Tarefa tar = (Tarefa) escravos.get(i).getInformacaoDinamicaProcessador().get(0);
                 int indexEscravo = metricaUsuarios.getUsuarios().indexOf(tar.getProprietario());
                 if (creditoTar > creditos.get(indexEscravo).GetCredito()) {
@@ -119,7 +179,7 @@ public class OSEP extends Escalonador {
                 }
             }
             return selec;
-        }
+     }
     }
 
     @Override
@@ -134,11 +194,14 @@ public class OSEP extends Escalonador {
         tarefaSelec = trf;
         if (trf != null) {
             CS_Processamento rec = escalonarRecurso();
+            
             if (rec != null) {
                 trf.setLocalProcessamento(rec);
                 trf.setCaminho(escalonarRota(rec));
                 mestre.enviarTarefa(trf);
                 contadorEscravos++;
+                creditos.get(metricaUsuarios.getUsuarios().indexOf(tarefaSelec.getProprietario())).AdidcionaAtual(rec.getPoderComputacional());                
+
             }
         }
     }
@@ -162,16 +225,27 @@ public class OSEP extends Escalonador {
     }
 
     private class CreditosUser {
-
+            
         private String usuario;
         private Double RecursoUsado;
         private Double RecursoDoado;
+        private Double PoderEmUso;
 
         public CreditosUser(String usuario) {
             this.usuario = usuario;
             this.RecursoDoado = 0.0;
             this.RecursoUsado = 0.0;
+            this.PoderEmUso = 0.0;
         }
+        
+        public void AdidcionaAtual(Double poder){
+            this.PoderEmUso +=  poder;
+        }
+        
+        public void RemoveAtual(Double poder){
+            this.PoderEmUso -=  poder;
+        }
+            
 
         public void AtualizaDoado(Double poder) {
             this.RecursoDoado += poder;
@@ -187,6 +261,10 @@ public class OSEP extends Escalonador {
 
         public String GetUsuario() {
             return usuario;
+        }
+        
+        public Double GetAtual(){
+            return this.PoderEmUso;
         }
     }
 }
