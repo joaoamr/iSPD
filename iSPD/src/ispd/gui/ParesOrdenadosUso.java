4cc55b59253176a3cbcb84e4a2076b5265320f10
denison <denison@e2395cfc-e4cf-471b-bddb-e734dad3db67>
2013-03-06 14:30:55 +0000

    git-svn-id: https://gspd.dcce.ibilce.unesp.br/ispd@193 e2395cfc-e4cf-471b-bddb-e734dad3db67

--- iSPD/src/ispd/gui/ParesOrdenadosUso.java
+++ iSPD/src/ispd/gui/ParesOrdenadosUso.java
@@ -9,25 +9,32 @@ package ispd.gui;
  * @author dancosta
  */
 //Classe que possui apenas doubles que indicam intervalos de tempos onde a máquina executou.
-public class ParesOrdenadosUso {
-    Double inicio, fim;
+public class ParesOrdenadosUso implements Comparable<ParesOrdenadosUso> {
+
+    Double inicio;
+    Double fim;
     //Construtor. Recebe o intervalo.
+
     public ParesOrdenadosUso(double inicio, double fim) {
         this.inicio = inicio;
         this.fim = fim;
     }
-    
-    public Double getInicio(){
+
+    public Double getInicio() {
         return this.inicio;
     }
-    
-    public Double getFim(){
+
+    public Double getFim() {
         return this.fim;
     }
-    
-    
-    public String toString(){
-        return inicio+" "+fim;
+
+    @Override
+    public String toString() {
+        return inicio + " " + fim;
     }
-    
+
+    @Override
+    public int compareTo(ParesOrdenadosUso o) {
+        return inicio.compareTo(o.inicio);
+    }
 }
