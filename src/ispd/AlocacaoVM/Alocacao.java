0952834536da5cb67dd085b7b929f9de512cd34b
Diogo Tavares <Diogo Tavares@diogots>
2014-12-03 02:05:04 -0200

    Pacote de definição da classe que implementa os algoritmos de alocação de vms

--- src/ispd/AlocacaoVM/Alocacao.java
+++ src/ispd/AlocacaoVM/Alocacao.java
@@ -6,6 +6,7 @@
 package ispd.AlocacaoVM;
 
 import ispd.motor.filas.servidores.CS_Processamento;
+import ispd.motor.filas.servidores.CentroServico;
 import ispd.motor.filas.servidores.implementacao.CS_VMM;
 import ispd.motor.filas.servidores.implementacao.CS_VirtualMac;
 import java.util.List;
@@ -14,9 +15,41 @@ import java.util.List;
  *
  * @author Diogo Tavares
  */
-public class Alocacao {
-    private List<CS_Processamento> maquinasFisicas;
-    private List<CS_VirtualMac> maquinasVirtuais;
-    private CS_VMM hypervisor;
+public abstract class Alocacao {
+    private List<CS_Processamento> maquinasFisicas; //lista de "escravos"
+    private List<CS_VirtualMac> maquinasVirtuais; //lista de vms "tarefas"
+    private CS_VMM hypervisor; //vmm responsável por implementar a política de alocação
     
+    protected List<List> caminhoMaquinas;
+    
+    //iniciar a alocação
+    public abstract void iniciar();
+    //selecionar o critério de seleção da vm
+    public abstract CS_VirtualMac escalonarVM();
+    //selecionar o critério de seleção do recurso
+    public abstract CS_Processamento escalonarRecurso();
+    //implementar a rota até o recurso selecionado
+    public abstract List<CentroServico> escalonarRota(CentroServico destino);
+    //realiza o escalonamento de fato
+    public abstract void escalonar();
+    
+    public abstract void migrarVM();
+    
+    public void addVM(CS_VirtualMac vm){
+        maquinasVirtuais.add(vm);
+    }
+
+    
+    /**
+     * Indica o intervalo de tempo utilizado pelo escalonador para realizar atualização dos dados dos escravos
+     * Retornar null para escalonadores estáticos, nos dinâmicos o método deve ser reescrito
+     * @return Intervalo em segundos para atualização
+     */
+    public Double getTempoAtualizar(){
+        return null;
+    }   
+    
+    
+
 }
+
