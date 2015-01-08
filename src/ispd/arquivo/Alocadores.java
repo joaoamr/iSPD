6ba62501b055096c31513fbadab8651c68b1e357
arthurjorge <arthurjorge@localhost.localdomain>
2015-01-08 16:43:08 -0200

    Adição do ModRR nos Alocadores

--- src/ispd/arquivo/Alocadores.java
+++ src/ispd/arquivo/Alocadores.java
@@ -45,7 +45,7 @@ public class Alocadores implements  ManipularArquivosAlloc{
      * guarda a lista de escalonadores implementados no iSPD, e que já estão
      * disponiveis para o usuario por padrão
      */
-    public final static String[] ALOCACAO = {"---", "RoundRobin", "Imediate"};
+    public final static String[] ALOCACAO = {"---", "RoundRobin", "Imediate", "ModRR"};
     /**
      * guarda a lista de escalonadores disponiveis
      */
